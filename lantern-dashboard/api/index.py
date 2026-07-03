6508a4bbede87cff8e8e012cb2d599c54b184ca0
Rob-Boss <bswardlick@gmail.com>
2026-07-03 13:11:51 -0700

    Expose app instance as direct top-level import in api/index.py

--- lantern-dashboard/api/index.py
+++ lantern-dashboard/api/index.py
@@ -1,4 +1,7 @@
-try:
-    from .main import app
-except ImportError:
-    from main import app
+import os
+import sys
+
+# Add the current folder to python path so 'main' can be resolved in any context
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from main import app
